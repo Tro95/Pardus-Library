615102d4adb75d0275a1d2bd0864f07b195ba1b6
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2024-03-29 23:13:08 +0000

    Refresh removals

--- src/classes/main/tile.js
+++ src/classes/main/tile.js
@@ -220,6 +220,7 @@ export default class Tile {
 
     removeHighlight(highlight_colour) {
         this.#highlights.delete(highlight_colour);
+        this.#refreshHighlightStatus();
     }
 
     isEmphasised() {
